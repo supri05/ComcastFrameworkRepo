d8d6858d247c5f82d9215e49aa96194f88ac560d
dell <dell@DESKTOP-3N0O4DU>
2024-06-12 17:23:30 +0530

    Changes

--- Selenium/src/test/java/Demo/Demo1.java
+++ Selenium/src/test/java/Demo/Demo1.java
@@ -1,5 +1,7 @@
 package Demo;
 
 public class Demo1 {
-System.out.println("Hello World");
+public static void main(String[] args) {
+	System.out.println();
+}
 }
---
+++ Selenium/src/test/java/practice/hometest/ProductLinks.java
@@ -0,0 +1,8 @@
+package practice.hometest;
+
+public class ProductLinks {
+public void test()
+{
+	System.out.println("Hi");
+}
+}
